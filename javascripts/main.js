582b47a59eb59e6ce725dccedd57ca7a1b95eca5
appostolov <appostolov@gmail.com>
2014-06-13 23:35:42 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,7 +5,7 @@ var w_w = window.innerWidth;
 
 setInterval(function(){
 	document.getElementById("title_t").style.left = poz+"px";
-	if(poz >= window.innerWidth - 300 || poz <= 0){
+	if(poz >= window.innerWidth - 330 || poz <= 0){
 		vel = -vel;
 	}
 	poz += vel;
