1fce3a82d902d5eee0f6f56006a4f71cb40b06be
appostolov <appostolov@gmail.com>
2014-06-14 00:58:11 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,11 +3,13 @@ var poz = 1;
 
 var w_w = window.innerWidth;
 
-setInterval(function(){
+var inter = setInterval(function(){
 	document.getElementById("title_t").style.left = poz+"px";
 	if(poz >= window.innerWidth - 330 || poz <= 0){
 		vel = -vel;
+		var x = Math.floor((Math.random() * 999) + 1);
+		var y = Math.random();
+		document.getElementById("abs_container").style.background = "#"+x.toString();
 	}
 	poz += vel;
 },17);
-
