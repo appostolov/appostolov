d217b1a3e961dcf7a11c4f06ffc65c0e1cc8225c
appostolov <appostolov@gmail.com>
2014-07-13 15:18:05 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,21 @@
 var vel = 5;
 var poz = 1;
+var colors = 255;
 
 var w_w = window.innerWidth;
 
 var inter = setInterval(function(){
 	document.getElementById("title_t").style.left = poz+"px";
+	document.getElementById("title_t").style.background = "rgba("+colors+","+colors+",0,1)";
 	if(poz >= window.innerWidth - 330 || poz <= 0){
-		document.getElementById("title_t").style.background = "gold";
+		colors = 255;
 		vel = -vel;
 		var x = Math.floor((Math.random() * 999) + 1);
 		document.getElementById("abs_container").style.background = "#"+x.toString();
 	}else{
-		document.getElementById("title_t").style.background = "black";
+		if(colors>=0){
+			colors --;	
+		}
 	}
 	poz += vel;
 },17);
