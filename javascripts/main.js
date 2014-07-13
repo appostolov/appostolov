88876f2f6dad870db2ca14bdd4f81d6acfa1705e
appostolov <appostolov@gmail.com>
2014-07-13 15:41:01 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -15,9 +15,13 @@ var time1 = 0;
 var inter = setInterval(function(){
 	if(time > 0 && path_num < paths.length){
 		for(var r = 0; r < paths.length){
-			paths[r].style.backgound = "gold";	
+			paths[r].style.backgound = "rgba(255,255,255,0.7)";	
 		}
 		paths[path_num].style.backgound = "gold";
+		if(time1 >= 3){
+			path_num += 1;
+			time1 = 0;
+		}
 	}
 	document.getElementById("title_t").style.left = poz+"px";
 	document.getElementById("title_t").style.background = "rgba("+colors+","+colors+",0,1)";
