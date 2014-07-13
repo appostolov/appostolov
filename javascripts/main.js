bf71d126467536b1b96193980901b71d73ae8335
appostolov <appostolov@gmail.com>
2014-07-13 15:06:50 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,12 +6,12 @@ var w_w = window.innerWidth;
 var inter = setInterval(function(){
 	document.getElementById("title_t").style.left = poz+"px";
 	if(poz >= window.innerWidth - 330 || poz <= 0){
-		document.getElementById("abs_container").style.color = "red";
+		document.getElementById("title_t").style.color = "red";
 		vel = -vel;
 		var x = Math.floor((Math.random() * 999) + 1);
 		document.getElementById("abs_container").style.background = "#"+x.toString();
 	}else{
-		document.getElementById("abs_container").style.color = "white";
+		document.getElementById("title_t").style.color = "white";
 	}
 	poz += vel;
 },17);
