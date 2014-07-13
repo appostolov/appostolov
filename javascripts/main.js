9aac8f626e3cdb6901145a51b362c06a24284487
appostolov <appostolov@gmail.com>
2014-07-13 15:57:47 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,6 +4,12 @@ var colors = 255;
 
 var w_w = window.innerWidth;
 
+var paths = document.getElementsByClassName("path");
+
+for(var a = 0; a < paths.length; a ++){
+	paths[a].style.background = "red";
+}
+
 
 var inter = setInterval(function(){
 	document.getElementById("title_t").style.left = poz+"px";
